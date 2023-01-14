9dd5f7e1f795e0bafbe10e685919d95960d0f384
Jeehyang Huh <jeehyang.huh@gmail.com>
2023-01-14 23:14:56 +0900

    change to hashrouter
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
